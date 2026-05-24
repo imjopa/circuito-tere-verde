1bfadcd39bf6677d2d257ec2cdf33802091ba84a
Leonardo Gurgel <leo@gurgel.io>
2026-05-24 12:36:36 -0300

    feat: adicionar react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
